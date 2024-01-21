b6d6fff48b1d7ece2d9bcae45e7cff26a68a5f59
osafalisayed <aousafalisayedb2@gmail.com>
2024-01-21 22:41:41 +0530

    Add models to admin

--- gsorcerer/admin.py
+++ gsorcerer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Organization, Categories, TechnologyTags, TopicTags
 # Register your models here.
+admin.site.register(Organization)
+admin.site.register(Categories)
+admin.site.register(TechnologyTags)
+admin.site.register(TopicTags)
